39deb07b5a52f121d72cb31d96da77e4473bcd4e
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2018-12-19 14:05:07 +0100

    Breaking: now tokenKit is a class, named TokenGenerator

--- test/token-kit-test.js
+++ test/token-kit-test.js
@@ -1,20 +1,20 @@
 /*
 	Token Kit
-	
-	Copyright (c) 2014 - 2016 Cédric Ronvel
-	
+
+	Copyright (c) 2014 - 2018 Cédric Ronvel
+
 	The MIT License (MIT)
-	
+
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
 	in the Software without restriction, including without limitation the rights
 	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 	copies of the Software, and to permit persons to whom the Software is
 	furnished to do so, subject to the following conditions:
-	
+
 	The above copyright notice and this permission notice shall be included in all
 	copies or substantial portions of the Software.
-	
+
 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,75 +24,73 @@
 	SOFTWARE.
 */
 
-/* jshint unused:false */
-/* global describe, it, before, after */
+/* global describe, it, before, after, expect */
 
 "use strict" ;
 
 
 
-var tokenKit = require( '../lib/token-kit.js' ) ;
-var expect = require( 'expect.js' ) ;
+var TokenGenerator = require( '..' ) ;
 
 
 
 
 
-			/* Tests */
+/* Tests */
 
 
 
-describe( "createToken()" , function() {
-	
-	it( "should create a token" , function() {
-		
+describe( "createToken()" , () => {
+
+	it( "should create a token" , () => {
+
 		var tokenStructure = [
 			{ key: 'expirationTime' , type: 'timestamp' , length: 4 } ,
 			{ key: 'increment' , type: 'increment16' } ,
 			{ key: 'random' , type: 'random' , length: 3 } ,
 			{ key: 'duration' , type: 'uint' , length: 2 } ,
-			{ key: 'type' , type: 'BASE36' , length: 2 } ,
+			{ key: 'type' , type: 'BASE36' , length: 2 }
 		] ;
-		
-		var gen = tokenKit( tokenStructure ) ;
-		
-		var test = function( data ) {
+
+		var gen = new TokenGenerator( tokenStructure ) ;
+
+		var test = data => {
 			console.log( "\n###\nData:" , data ) ;
 			var token = gen.create( data ) ;
 			console.log( "Data after create():" , data ) ;
 			console.log( "Token:" , token ) ;
 			var extracted = gen.extract( token ) ;
 			console.log( "Extracted:" , extracted ) ;
-			expect( extracted ).to.eql( data ) ;
+			expect( extracted ).to.equal( data ) ;
 		} ;
-		
+
 		test( { duration: 300 , type: 'H' } ) ;
 		test( { duration: 600 , type: 'QS' } ) ;
 		test( { duration: 3600 , type: 'CK' } ) ;
 	} ) ;
-	
-	it( "should create a token²" , function() {
-		
+
+	it( "should create a token²" , () => {
+
 		var tokenStructure = [
 			{ key: 'expirationTime' , type: 'timestamp' , length: 4 } , // in seconds
 			{ key: 'userId' , type: 'hex' , length: 12 } ,  // MongoId: 12 bytes, 24 hex chars
 			{ key: 'agentId' , type: 'hex' , length: 5 } ,  // 10 hex chars
-			{ key: 'increment' , type: 'increment8' } ,   
+			{ key: 'increment' , type: 'increment8' } ,
 			{ key: 'type' , type: 'BASE36' , length: 2 }
 		] ;
-		
-		var gen = tokenKit( tokenStructure ) ;
-		
-		var test = function( data ) {
+
+		var gen = new TokenGenerator( tokenStructure ) ;
+
+		var test = data => {
 			console.log( "\n###\nData:" , data ) ;
 			var token = gen.create( data ) ;
 			console.log( "Data after create():" , data ) ;
 			console.log( "Token:" , token ) ;
 			var extracted = gen.extract( token ) ;
 			console.log( "Extracted:" , extracted ) ;
-			expect( extracted ).to.eql( data ) ;
+			expect( extracted ).to.equal( data ) ;
 		} ;
-		
+
 		test( { expirationTime: 300 , type: 'H' , userId: '0123456789abcdef01234567' , agentId: '0123456789' } ) ;
 		test( { expirationTime: 600 , type: 'QS' , userId: '0123456789abcdef01234567' , agentId: '0123456789' } ) ;
 		test( { expirationTime: 3600 , type: 'CK' , userId: '0123456789abcdef01234567' , agentId: '0123456789' } ) ;
